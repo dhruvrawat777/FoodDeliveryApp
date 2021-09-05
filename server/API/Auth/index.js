b7cb81cde9ccfd95040a74d4f7506162b129c6f7
Dhruv <dhruvrawat2611@gmail.com>
2021-09-05 10:34:43 +0530

    Signup route done

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -11,7 +11,7 @@ const Router = express.Router();
 
 /*
 Route   /signup
-Desc    signup with email and password
+Desc    Register new user
 Params  none
 Access  Public
 Method  POST
@@ -19,22 +19,20 @@ Method  POST
 
 Router.post("/signup", async (req, res) => {
     try {
-        const { email, password, fullname, phoneNumber } = req.body.credentials;
+        //const { email, password, fullname, phoneNumber } = req.body.credentials;
 
         //check if email exists
-        await UserModel.findByEmailAndPhone(email,phoneNumber);
-        
+        await UserModel.findByEmailAndPhone(req.body.credentials);
+
         //hash password
-        const bcryptSalt = await bcrypt.genSalt(8);
-        const hashedPassword = await bcrypt.hash(password, bcryptSalt);
+       // const bcryptSalt = await bcrypt.genSalt(8);
+        //const hashedPassword = await bcrypt.hash(password, bcryptSalt);
 
         //save to dB
-        await UserModel.create(
-            { ...req.body.credentials, password: hashedPassword }
-        );
+        const newUser = await UserModel.create(req.body.credentials);
 
         //generate JWT auth token
-        const token = jwt.sign({ user: { fullname, email } }, "ZomatoAPP");
+        const token = newUser.generateJwtToken();
 
 
         //respond
@@ -45,6 +43,13 @@ Router.post("/signup", async (req, res) => {
     }
 });
 
+/*
+Route   /signin
+Desc    signup with email and password
+Params  none
+Access  Public
+Method  POST
+*/
 
 
 
--- server/database/user/index.js
+++ server/database/user/index.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema({
     fullname: { type: String, required: true },
@@ -13,7 +14,7 @@ const UserSchema = new mongoose.Schema({
     }
 );
 
-UserSchema.statics.findByEmailAndPhone = async (email, phoneNumber) => {
+UserSchema.statics.findByEmailAndPhone = async ({ email, phoneNumber }) => {
     const checkUserByEmail = await UserModel.findOne({ email });
     const checkUserByPhone = await UserModel.findOne({ phoneNumber });
     if (checkUserByEmail || checkUserByPhone) {
@@ -21,6 +22,10 @@ UserSchema.statics.findByEmailAndPhone = async (email, phoneNumber) => {
     }
     return false;
 };
+
+UserSchema.methods.generateJwtToken = function () {
+    return jwt.sign({ user: this._id.toString() }, "ZomatoAPP");
+}
 
 UserSchema.pre("save", function (next) {
     const user = this;
