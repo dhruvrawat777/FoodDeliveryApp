86d3fcfb3bc6f513df07ff39ddd52577b4c08bc6
Dhruv <dhruvrawat2611@gmail.com>
2021-09-20 10:00:22 +0530

    Frontend

--- client/src/Layout/Checkout.layout.jsx
+++ client/src/Layout/Checkout.layout.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
-// Components
-import Navbar from "../Components/Navbar";
-import FoodTab from "../Components/FoodTab";
-
+// components
+import Navbar from "../Components/Navbar/checkoutNavbar";
 // redux action
 import { getCart } from "../Redux/Reducer/Cart/Cart.action";
 
-const HomeLayout = (props) => {
+const CheckoutLayout = (props) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -18,10 +16,9 @@ const HomeLayout = (props) => {
   return (
     <>
       <Navbar />
-      <FoodTab />
       <div className="container mx-auto px-4 lg:px-20 ">{props.children}</div>
     </>
   );
 };
 
-export default HomeLayout;
+export default CheckoutLayout;
